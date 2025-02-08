60a7c2e402f7bcca34f4a33e437bba1e09ec0780
prachirahud <prahud@gmail.com>
2025-02-08 17:47:39 +0530

    Updated Contact Us form with backend

--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/contactRoutes.js
-import express from "express";
-import { createContact} from "../controllers/contactControler.js";
-
-const router = express.Router();
-router.post("/contactus", createContact);
-  
-
-export default router;
